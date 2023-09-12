2ca0f8aac227b57cf9a1f1f5c97a1e1ff0e95f4f
Kipleting Edwin <kipletingedwin4@gmail.com>
2023-09-12 16:29:53 +0300

    add xbar and menu bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
 import bar from "../assets/bars-solid.svg"
+import xbar from "../assets/xmark-solid.svg"
 
 function NavBar() {
+
+  const [openMenu, setOpenMenu] = useState(false);
   return <nav className="navbar">
     <a className="port" href="/"> Portfolio</a>
     <div className="menu"> 
-    <img className="bar" alt="bar" src={bar}/>
+    <img className="bar" alt="bar" src={ openMenu ? xbar : bar}  onClick={() => setOpenMenu(!openMenu)}/>
         <ul >
             <li><a href="#about"> About </a></li>
             <li><a href="#skills"> Skills </a></li>
