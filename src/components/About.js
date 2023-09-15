96f439318a1956a566d52fb3a3c3ca28715bcd43
Kipleting Edwin <kipletingedwin4@gmail.com>
2023-09-15 15:33:53 +0300

    add contact me button

--- src/components/About.js
+++ src/components/About.js
@@ -3,13 +3,15 @@ import edwin from '../assets/edwin2.png'
 
 function About() {
   return (
-    <section>
-        <div>
+    <section className='container'>
+        <div className='content'>
             <h1> Hello, I'm Edwin</h1>
             <p> I'm an entry level full-stack developer using React and Ruby on Rails. React out if you like to learn more! </p>
         </div>
-        <a href='mailto:myemail@email.com'> Contact Me</a>
+        <a href='mailto:myemail@email.com' className='contactbtn'> Contact Me</a>
         <img src={ edwin} alt='me'/>
+        <div className='topblur'></div>
+        <div className='bottomblur'></div>
     </section>
     
   )
