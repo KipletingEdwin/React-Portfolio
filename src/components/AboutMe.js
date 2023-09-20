6d6ee14c24f5f753ed56f5bfba039b6a89d767a1
Kipleting Edwin <kipletingedwin4@gmail.com>
2023-09-20 10:00:39 +0300

    add an aboutme section

---
+++ src/components/AboutMe.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function AboutMe() {
+  return (
+    <section>
+        <h2> About</h2>
+        <div>
+            <img src='' alt='me'/>
+        </div>
+    </section>
+    
+  )
+}
+
+export default AboutMe
