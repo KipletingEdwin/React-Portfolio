e8558df1f030f716800a60f74339d006d9f4c464
Kipleting Edwin <kipletingedwin4@gmail.com>
2023-09-28 10:23:33 +0300

    style my work history

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,47 +1,51 @@
-import React from 'react'
-import skills from '../data/skills.json'
-import history from '../data/history.json'
+import React from "react";
+import skills from "../data/skills.json";
+import history from "../data/history.json";
 
 function Experience() {
   return (
-    <section id='experience'>
-        <h2> Experience </h2>
-        <div> 
-            <div> {
-                skills.map((skill, id) => {
-                    return <div key={id}>
-                        <div> <img src={skill.imageSrc}  alt={skill.title}/> </div>
+    <section id="experience" className="exContainer" >
+      <h2> Experience </h2>
+      <div className="exContent" >
+        <div className="skillSection" >
+          {" "}
+          {skills.map((skill, id) => {
+            return (
+              <div key={id} className="skill" >
+                <div className="myImageContainer" >
+                  {" "}
+                  <img src={skill.imageSrc} alt={skill.title} />{" "}
+                </div>
 
-                        <p>{skill.title}</p>
-
-                    </div>
-                   
-                })
-                } </div>
-            <ul>
-                {
-                    history.map((historyItem, id) =>{
-                        return <li key={id}> 
-                        <img src={historyItem.imageSrc} alt={`${historyItem.organisation} Logo`} />
-                        <div> 
-                            <h3>{`${historyItem.role}, ${historyItem.organisation}`} </h3>
-                            <p> {`${historyItem.startDate} - ${historyItem.endDate}`}  </p>
-                            <ul> 
-                                {historyItem.experiences.map(( experience, id) =>{
-                                return <li key={id}> {experience} </li>;
-                            })} </ul>
-                        </div>
-                        </li>
-                    })
-                }
-            </ul>
+                <p>{skill.title}</p>
+              </div>
+            );
+          })}{" "}
         </div>
-        
-
-
+        <ul>
+          {history.map((historyItem, id) => {
+            return (
+              <li key={id}>
+                <img
+                  src={historyItem.imageSrc}
+                  alt={`${historyItem.organisation} Logo`}
+                />
+                <div className="historyItemDetails">
+                  <h3>{`${historyItem.role}, ${historyItem.organisation}`} </h3>
+                  <p> {`${historyItem.startDate} - ${historyItem.endDate}`} </p>
+                  <ul>
+                    {historyItem.experiences.map((experience, id) => {
+                      return <li key={id}> {experience} </li>;
+                    })}{" "}
+                  </ul>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </section>
-   
-  )
+  );
 }
 
-export default Experience
+export default Experience;
